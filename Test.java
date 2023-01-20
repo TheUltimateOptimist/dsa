db7fc7ca6bc374f65ac46ba692ce011e17a7f206
TheUltimateOptimist <digitalConfidence@web.de>
2023-01-20 12:52:20 +0100

    programming assignment

--- Test.java
+++ Test.java
@@ -2,26 +2,31 @@ import elementary_sorting.Insertion;
 import elementary_sorting.LinearShuffle;
 import elementary_sorting.Selection;
 import elementary_sorting.ShellSort;
+import advanced_sorting.Point;
 
 
 public class Test {
     public static void main(String[] args){
-        Comparable[] unsortedArray = {9,8,7,6,5,4,3,2,1};
-        System.out.println("Unsorted: " + Util.toString(unsortedArray));
-        Comparable[] workingArray = unsortedArray.clone();
-        Selection.sort(workingArray);
-        System.out.println("Sorted: " + Util.toString(workingArray));
-        workingArray = unsortedArray.clone();
-        System.out.println("Unsorted: " + Util.toString(workingArray));
-        Insertion.sort(workingArray);
-        System.out.println("Sorted: " + Util.toString(workingArray));
-        workingArray = unsortedArray.clone();
-        System.out.println("Unsorted: " + Util.toString(workingArray));
-        ShellSort.sort(workingArray);
-        System.out.println("Sorted: " + Util.toString(workingArray));
-        workingArray = unsortedArray.clone();
-        System.out.println("Unshuffled: " + Util.toString(workingArray));
-        LinearShuffle.shuffle(workingArray);
-        System.out.println("Shuffled: " + Util.toString(workingArray));
+        // Comparable[] unsortedArray = {9,8,7,6,5,4,3,2,1};
+        // System.out.println("Unsorted: " + Util.toString(unsortedArray));
+        // Comparable[] workingArray = unsortedArray.clone();
+        // Selection.sort(workingArray);
+        // System.out.println("Sorted: " + Util.toString(workingArray));
+        // workingArray = unsortedArray.clone();
+        // System.out.println("Unsorted: " + Util.toString(workingArray));
+        // Insertion.sort(workingArray);
+        // System.out.println("Sorted: " + Util.toString(workingArray));
+        // workingArray = unsortedArray.clone();
+        // System.out.println("Unsorted: " + Util.toString(workingArray));
+        // ShellSort.sort(workingArray);
+        // System.out.println("Sorted: " + Util.toString(workingArray));
+        // workingArray = unsortedArray.clone();
+        // System.out.println("Unshuffled: " + Util.toString(workingArray));
+        // LinearShuffle.shuffle(workingArray);
+        // System.out.println("Shuffled: " + Util.toString(workingArray));
+
+        Point one = new Point(2, 0);
+        Point two = new Point(0, 0);
+        System.out.println(one.slopeTo(two));
     }
 }
