50aa4ef0a3e839a1fdc16c31bd9c030bae614414
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-18 13:32:12 +0100

    added UnorderedListMap implementation

---
+++ elementary_symbol_tables/unordered_list_map.dart
@@ -0,0 +1,57 @@
+import 'custom_map.dart';
+
+class Pair<Key, Value>{
+  Pair(this.key, this.value);
+  Key key;
+  Value value;
+
+  @override
+  String toString() {
+    return "$key:$value";
+  }
+}
+
+class UnorderedListMap<Key, Value> extends CustomMap<Key, Value>{
+  final content = List<Pair<Key, Value>>.empty(growable: true);
+  @override
+  get(key) {
+    for(var pair in content){
+      if(pair.key == key){
+        return pair.value;
+      }
+    }
+    return null;
+  }
+
+  @override
+  bool hasKey(key) => content.any((pair) => pair.key == key);
+
+  @override
+  bool get isEmpty => content.isEmpty;
+
+  @override
+  Iterable<Key> get keys => content.map<Key>((pair) => pair.key);
+
+  @override
+  void put(key, value){
+     for(var pair in content){
+      if(pair.key == key){
+        pair.value = value;
+        return;
+      }
+    }
+    content.add(Pair(key, value));
+  }
+
+  @override
+  void remove(key) => content.removeWhere((pair) => pair.key == key);
+
+  @override
+  int get size => content.length;
+
+
+  @override
+  String toString() {
+    return "UnorderedListMap($content)";
+  }
+}
