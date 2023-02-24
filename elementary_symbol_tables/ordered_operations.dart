037235ccb2f17a5cb45971e3a5e06c3778d85d2c
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-24 10:24:51 +0100

    added ordered operations

---
+++ elementary_symbol_tables/ordered_operations.dart
@@ -0,0 +1,30 @@
+abstract class OrderedOperations<Key extends Comparable, Value>{
+  ///smallest key
+  Key min();
+
+  ///largest key
+  Key max();
+
+  ///largest key less than or equal to key
+  Key floor(Key key);
+
+  ///smallest key greater than or equal to key
+  Key ceiling(Key key);
+
+  ///number of keys less than key
+  int rank(Key key);
+
+  ///key of rank k
+  Key select(int k);
+
+  ///delete smallest key
+  void deleteMin();
+
+  ///delete largest key
+  void deleteMax();
+
+  ///number of keys in [low..high]
+  ///
+  ///low and high are inclusive
+  Iterable<Key> keys(Key low, Key high);
+}
